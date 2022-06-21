1612224de38b9a1abfc11c0cb484eb521f65ff36
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-21 14:42:34 +0430

    edit TestOneInnerClassApplication

--- codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/testOneCollection/TestOneInnerClassApplication.java
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/testOneCollection/TestOneInnerClassApplication.java
@@ -4,6 +4,7 @@ public class TestOneInnerClassApplication {
     public static void main(String[] args) {
         Outer outer = new Outer();
         outer.display();
+
 //        Outer.Inner inner = outer.new Inner();
     }
 
