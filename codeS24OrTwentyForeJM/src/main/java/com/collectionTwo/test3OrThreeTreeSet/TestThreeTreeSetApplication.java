612899da44c207c2a381f06fb55ebbc2f7193d93
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-21 23:14:44 +0430

    edit TestThreeTreeSetApplication

--- codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/test3OrThreeTreeSet/TestThreeTreeSetApplication.java
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/test3OrThreeTreeSet/TestThreeTreeSetApplication.java
@@ -11,11 +11,18 @@ public class TestThreeTreeSetApplication {
         treeSetOne.add(6);
         treeSetOne.add(40);
         treeSetOne.add(12);
+        treeSetOne.add(4);
+        treeSetOne.add(26);
+        treeSetOne.add(86);
         System.out.println(treeSetOne);
         System.out.println("First : " + treeSetOne.first());
         System.out.println("Last : " + treeSetOne.last());
         System.out.println("Higher : " + treeSetOne.higher(8));
         System.out.println("Lower : " + treeSetOne.lower(16));
+        System.out.println("Head set : " + treeSetOne.headSet(15));
+        System.out.println("Tail set : " + treeSetOne.tailSet(12));
+        System.out.println("Sub set : " + treeSetOne.subSet(4, 26));
+
 
         System.out.println();
 
