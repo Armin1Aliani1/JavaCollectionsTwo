083af8d29edea26f0c2ea2a777ed1192830b0c21
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-21 14:35:40 +0430

    add TestOneInnerClassApplication and Outer

---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/testOneCollection/Outer.java
@@ -0,0 +1,20 @@
+package com.collectionTwo.testOneCollection;
+
+public class Outer {
+
+    private int number = 200;
+
+    public void display() {
+        System.out.println("Inside Outer class # display()");
+        Inner innerObject = new Inner();
+        innerObject.print();
+    }
+
+    class Inner {
+        private void print() {
+            System.out.println("Inside Inner class # print()");
+            System.out.println("This is number in Outer class but we call in inner class : " + number);
+        }
+    } // End of Inner class
+
+} // End of Outer class
---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/testOneCollection/TestOneInnerClassApplication.java
@@ -0,0 +1,10 @@
+package com.collectionTwo.testOneCollection;
+
+public class TestOneInnerClassApplication {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.display();
+//        Outer.Inner inner = outer.new Inner();
+    }
+
+}
