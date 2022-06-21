1a37ec9fdc7b30593f990285e89d71b1fac18d50
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-22 00:11:26 +0430

    add TestForeTreeSetApplication and Student

---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/test4OrForeTreeSet/Student.java
@@ -0,0 +1,30 @@
+package com.collectionTwo.test4OrForeTreeSet;
+
+public class Student implements Comparable<Student> {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Student() {
+    }
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return id - o.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/test4OrForeTreeSet/TestForeTreeSetApplication.java
@@ -0,0 +1,43 @@
+package com.collectionTwo.test4OrForeTreeSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+
+public class TestForeTreeSetApplication {
+    public static void main(String[] args) {
+
+        Student studentOne = new Student(100, "Armin", "Aliani");
+        Student studentTwo = new Student(250, "Arian", "Aliani");
+        Student studentThree = new Student(120, "Reza", "Aliani");
+        Student studentFore = new Student(14, "Mahdi", "Aliani");
+
+        TreeSet<Student> treeSetOne = new TreeSet<>();
+        treeSetOne.add(studentOne);
+        treeSetOne.add(studentTwo);
+        treeSetOne.add(studentThree);
+        treeSetOne.add(studentFore);
+
+        System.out.println(treeSetOne);
+
+        System.out.println();
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(studentOne);
+        studentList.add(studentTwo);
+        studentList.add(studentThree);
+        studentList.add(studentFore);
+
+        System.out.println();
+
+        System.out.println("Before sorting : " + studentList);
+
+        Collections.sort(studentList);
+
+        System.out.println();
+
+        System.out.println("After sorting : " + studentList);
+
+    }
+}
