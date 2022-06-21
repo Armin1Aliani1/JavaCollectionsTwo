b5668a94886f52ebb2b05fc7cd690ca6893104e3
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-21 14:57:52 +0430

    edit TestTwoInnerClassApplication and OuterTwo

---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/testTwoCollection/OuterTwo.java
@@ -0,0 +1,20 @@
+package com.collectionTwo.testTwoCollection;
+
+public class OuterTwo {
+
+    private int number = 200;
+
+    public void display() {
+        System.out.println("Inside Outer class # display()");
+        Inner innerObject = new Inner();
+        innerObject.print();
+    }
+
+    static class Inner {
+        public void print() {
+            System.out.println("Inside Inner class # print()");
+//            System.out.println("This is number in Outer class but we call in inner class : " + number);
+        }
+    } // End of Inner class
+
+} // End of Outer class
---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/testTwoCollection/TestTwoInnerClassApplication.java
@@ -0,0 +1,9 @@
+package com.collectionTwo.testTwoCollection;
+
+public class TestTwoInnerClassApplication {
+    public static void main(String[] args) {
+        OuterTwo.Inner inner = new OuterTwo.Inner();
+        inner.print();
+    }
+
+}
