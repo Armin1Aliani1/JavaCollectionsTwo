b23eb0602e434dc2f9f94a5d53ea44fb3e8150d6
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-22 12:30:37 +0430

    add TestSixCompareToApplication

---
+++ codeS24OrTwentyForeJM/src/main/java/com/collectionTwo/test6OrSixCompareTo/TestSixCompareToApplication.java
@@ -0,0 +1,11 @@
+package com.collectionTwo.test6OrSixCompareTo;
+
+public class TestSixCompareToApplication {
+    public static void main(String[] args) {
+        String strOne = "Armin";
+        String strTwo = "Arian";
+        String strThree = "Armin";
+        String strFore = "Reza";
+        System.out.println(strOne.compareToIgnoreCase(strTwo));
+    }
+}
